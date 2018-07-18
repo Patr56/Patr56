5b2fc2923395bccace66448110f22efdef85aecc
Pavel Horoshilov <phoroshilov@yandex.ru>
2018-07-18 12:25:51 +0300

    js refactor

--- js/main.js
+++ js/main.js
@@ -3,13 +3,17 @@
     var currentMonth = new Date().getMonth();
     var placeForSeasonEl = document.getElementById(idElement);
 
-    if ([0, 1, 11].indexOf(currentMonth)) {
+    function checkSeason(months) {
+        return months.indexOf(currentMonth) >= 0;
+    }
+
+    if (checkSeason([0, 1, 11])) {
         season = "winter";
-    } else if ([2, 3, 4].indexOf(currentMonth)) {
+    } else if (checkSeason([2, 3, 4])) {
         season = "spring";
-    } else if ([5, 6, 7].indexOf(currentMonth)) {
+    } else if (checkSeason([5, 6, 7])) {
         season = "summer";
-    } else if ([8, 9, 10].indexOf(currentMonth)) {
+    } else if (checkSeason([8, 9, 10])) {
         season = "autumn";
     } else {
         season = null;
